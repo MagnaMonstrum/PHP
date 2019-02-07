c4952f7a59789e78bef6db54a97022410440b256
Mo <midbid@roc-dev.com>
2019-02-07 10:59:49 +0100

    Beginners cursus PHP en OO PHP

--- PretPark/index.php
+++ PretPark/index.php
@@ -3,7 +3,6 @@
     include_once("lib/Pretpark.php");
     $Pretpark = new Pretpark();
     $Pretpark->selectAttracties();
-
 ?>
 
 <div class="container">
@@ -11,12 +10,12 @@
         <h1 class="mb-5">Attractieoverzicht</h1>
     </div>
 
-<?php foreach ($Pretpark->getAttracties() as $advertentie){?>
+<?php foreach ($Pretpark->getAttracties() as $attractie){?>
     <div class="row">
-    <div class="col-md-8 mb-2"><img src="<?php echo $advertentie->getUrlFoto();?>" alt="Attractieplaatje"></div>
+    <div class="col-md-8 mb-2"><img src="<?php echo $attractie->getUrlFoto();?>" alt="Attractieplaatje"></div>
         <div class="col-md-4">
-        <h3><?php echo $advertentie->getAttractieNaam();?></h3>
-        <p><?php echo $advertentie->getAttractieOmschrijving();?></p>
+        <h3><?php echo $attractie->getAttractieNaam();?></h3>
+        <p><?php echo $attractie->getAttractieOmschrijving();?></p>
         </div>
     </div>
 <?php } ?>
